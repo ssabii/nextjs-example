d12c4c4ebdd23b19844ea2eb03e14024c7f0780f
ssabii <fhy1930@gmail.com>
2022-04-30 22:49:59 +0900

    Custom App

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,7 @@ import NavBar from "../components/NavBar";
 export default function Home() {
     return (
         <div>
-            <NavBar />
-            <h1 className="active">Home</h1>
-            <style jsx>{`
-                a {
-                    color: white;
-                }
-            `}</style>
+            <h1>Home</h1>
         </div>
     );
 }
