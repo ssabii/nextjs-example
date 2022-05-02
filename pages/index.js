67a189c62602e0dd75bfdbb11192c60ee2484b71
ssabii <fhy1930@gmail.com>
2022-05-02 21:27:22 +0900

    Redirec and Rewrite

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/seo";
 
-const API_KEY = "002832db5edc86be1c2adc7e74e0a024";
-
 export default function Home() {
     const [movies, setMovies] = useState();
 
     useEffect(() => {
         (async () => {
-            const { results } = await (
-                await fetch(
-                    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-                )
-            ).json();
+            const { results } = await (await fetch(`api/movies`)).json();
             setMovies(results);
         })();
     }, []);
